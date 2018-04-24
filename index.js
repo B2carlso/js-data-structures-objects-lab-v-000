5f128ded8d7b6794e806c7da7efbd0ed31b98da8
Brad Carlson <b2carlso@yahoo.com>
2018-04-24 01:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-let driver = {
-
-}
+let driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign({}, driver, {[key]: value} );
+  return Object.assign({}, driver, {[key]: value});
 }
